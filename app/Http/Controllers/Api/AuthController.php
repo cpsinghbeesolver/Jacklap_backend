940d46f88873e336db17007705c4732262ab6c6a
nitish <nitish@beesolvertechnology.in>
2026-09-18 17:03:08 +0530

    changes done for login issue fixed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -150,7 +150,7 @@ class AuthController extends Controller
         if($user->email_verified_at){
             $isloggedIn = 'true';
         }
-        if ($role === 'provider') {
+        if ($role === 'provider' && $user->profile_step < 3) {
             $response->cookie('is_auth_incomplete', 'true', 120, '/', null, true, false);
         } else {
             $response->withoutCookie('is_auth_incomplete');
